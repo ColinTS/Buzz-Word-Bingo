98538a3e66f55957484fb28cf3f771295a4306eb
ctsygiel <ctsygiel@gmail.com>
2017-04-18 20:40:32 -1000

    PUT is passing

--- routes/buzzwords.js
+++ routes/buzzwords.js
@@ -17,11 +17,24 @@ router.route('/')
      } else {
       buzzWordCollection.push({'buzzWord': req.body.buzzWord, 'points': Number(req.body.points),
       'heard': false, 'newScore': 0});
-      console.log(buzzWordCollection);
       res.send({success: true});
     }
   })
   .put((req, res) => {
+    if(buzzWordCollection.map((x) => {
+      return x.buzzWord;}).indexOf(req.body.buzzWord) === -1){
+      res.send({success: false});
+    } else {
+      buzzWordCollection[buzzWordCollection.map((x) => {
+      return x.buzzWord;}).indexOf(req.body.buzzWord)].heard = true;
+
+      buzzWordCollection[buzzWordCollection.map((x) => {
+      return x.buzzWord;}).indexOf(req.body.buzzWord)].newScore += buzzWordCollection[buzzWordCollection.map((x) => {
+      return x.buzzWord;}).indexOf(req.body.buzzWord)].points;
+
+      res.send({'success': true, 'newScore': buzzWordCollection[buzzWordCollection.map((x) => {
+      return x.buzzWord;}).indexOf(req.body.buzzWord)].newScore });
+    }
 
   })
   .delete((req, res) => {
